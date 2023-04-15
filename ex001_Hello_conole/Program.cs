32f1574ef4e2e0f0fe05a3b23bbbc98f10d7a956
Anton_Yushin <ya.napishimne@yandex.ru>
2023-04-15 12:37:13 +0300

    ex005

--- ex001_Hello_conole/Program.cs
+++ ex001_Hello_conole/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("ura, this is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
